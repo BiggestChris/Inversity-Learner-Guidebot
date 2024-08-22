d0fcad34ae18ad188458a37865155383d97c0809
BiggestChris <stylianouchristopher8@gmail.com>
2024-08-22 20:36:48 +0100

    Final changes
    Changes to results page and some UX changes for the Chatbot (and prompts)

--- functions_GPT.py
+++ functions_GPT.py
@@ -4,7 +4,6 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from requests.exceptions import RequestException
 
-# TODO: Add error-handling to the below ChatGPT API calls
 
 load_dotenv()
 
@@ -22,6 +21,9 @@ def comprehend_data(input_data, mark_scheme):
                     that you will be given and compare to the mark scheme. You can find a markscheme here {mark_scheme}. Please then write
                     a short paragraph with feedback and guidance on whether it can be submitted to Inversity (it DOES NOT need to be at production level
                     to be ready to submit, just at proof of concept stage). Keep the total paragraph short, not more than 4 sentences.
+                    So please put it in the format of 'Stage: (Then fill in what stage it is at as per the markscheme).' and then on a new line a paragraph with
+                    'Feedback: (Then provide encouraging and specific feedback here on how to improve).' and then on another new line 'Status: (Mark as 'Ready to submit' unless they user has
+                    written less than 100 lines of code across the repo). 
                     """},
                     {"role": "user",
                     "content": input_data }
